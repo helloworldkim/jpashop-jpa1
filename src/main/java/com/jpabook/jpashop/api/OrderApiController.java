abce3d44da546296e8ab12d6b57bec89b785685a
kcgi <kcgi@zlgoon.co.kr>
2023-02-16 10:49:47 +0900

    [COMMIT] - 이전에 rollback했던부분 다시 추가

---
+++ src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,32 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1() {
+
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+
+    }
+}
--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,7 @@ package com.jpabook.jpashop.domain;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Member {
     private String name;
     @Embedded
     private Address address;
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
